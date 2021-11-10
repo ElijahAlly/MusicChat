92511a97eb972168f4624765d30e5bab0291e5de
Elijah Ally <elijahally18@gmail.com>
2021-11-10 12:39:10 -0500

    handle push errors

--- pages/generate-anonymous-username.tsx
+++ pages/generate-anonymous-username.tsx
@@ -12,67 +12,76 @@ import UsernameGuidelines from '../components/username_guidelines';
 import checkUsernameGuidelines from '../lib/check_username_guidelines';
 
 const config: Config = {
-    dictionaries: [adjectives, colors, animals],
-    separator: '_',
+	dictionaries: [adjectives, colors, animals],
+	separator: '_',
 };
 
 function GenerateAnonymousUsername() {
-    const [separator, setSeparator] = useState('_');
+	const [separator, setSeparator] = useState('_');
 	const [username, setUsername] = useState(uniqueNamesGenerator(config));
 
-    const handleUnderscores = (username: string) => {
-        if (separator === '_') return username;
+	const handleUnderscores = (username: string) => {
+		if (separator === '_') return username;
 
-        let newUsername = '';
+		let newUsername = '';
 
-        for (let i = 0; i < username.length; i++) {
-            if (username[i] === '_') {
-                continue;
-            }
+		for (let i = 0; i < username.length; i++) {
+			if (username[i] === '_') {
+				continue;
+			}
 
-            newUsername += username[i];
-        }
+			newUsername += username[i];
+		}
 
-        return newUsername;
-    }   
+		return newUsername;
+	};
 
-    let lengthCheck: HTMLElement | null = null;
-    let specialCharsCheck: HTMLElement | null = null;
+	let lengthCheck: HTMLElement | null = null;
+	let specialCharsCheck: HTMLElement | null = null;
 
-    useEffect(() => {
-        lengthCheck = document.getElementById('two-chars-long');
-        specialCharsCheck = document.getElementById('no-special-chars');
-    })
+	useEffect(() => {
+		lengthCheck = document.getElementById('two-chars-long');
+		specialCharsCheck = document.getElementById('no-special-chars');
+	});
 
 	return (
 		<>
 			<Navbar loggedIn={false} />
 			<section className='anonymous-section'>
 				<form className='anonymous-form'>
-                    <UsernameGuidelines />
+					<UsernameGuidelines />
 					<input
 						type='text'
 						className='form-input'
 						onChange={(e) => {
-                            setUsername(e.currentTarget.value);
-                            checkUsernameGuidelines(e.currentTarget.value, lengthCheck, specialCharsCheck);
-                        }}
+							setUsername(e.currentTarget.value);
+							checkUsernameGuidelines(
+								e.currentTarget.value,
+								lengthCheck,
+								specialCharsCheck
+							);
+						}}
 						value={handleUnderscores(username)}
-                        id='username-input'
+						id='username-input'
 					/>
-                    <div className='btn-container'>
-                        <button
-                            className='submit-btn'
-                            onClick={(e) => {
-                                e.preventDefault();
-                                setUsername(uniqueNamesGenerator(config));
-                            }}>
-                            Generate A Random Username
-                        </button>
-                        <button className='submit-btn underscore-btn' onClick={(e) => {e.preventDefault();setSeparator((prev) => prev === '_' ? '' : '_')}}>
-                            {separator === '_' ? 'Without' : 'With'} underscores {`"_"`}
-                        </button>
-                    </div>
+					<div className='btn-container'>
+						<button
+							className='submit-btn'
+							onClick={(e) => {
+								e.preventDefault();
+								setUsername(uniqueNamesGenerator(config));
+							}}>
+							Generate A Random Username
+						</button>
+						<button
+							className='submit-btn underscore-btn'
+							onClick={(e) => {
+								e.preventDefault();
+								setSeparator((prev) => (prev === '_' ? '' : '_'));
+							}}>
+							{separator === '_' ? 'Without' : 'With'} underscores {`"_"`}
+						</button>
+					</div>
 				</form>
 				<Link href='/'>
 					<a className='auth-btn'>Continue</a>
@@ -80,6 +89,6 @@ function GenerateAnonymousUsername() {
 			</section>
 		</>
 	);
-};
+}
 
 export default GenerateAnonymousUsername;
